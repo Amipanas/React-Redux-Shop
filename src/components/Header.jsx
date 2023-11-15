9a6c2246e25f7610244cee565f721c77e42b36df
Amirparsa <96636236+Amipanas@users.noreply.github.com>
2023-11-16 02:42:26 +0330

    Updating Header Component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 const Header = () => {
   return (
     <div className="container">
-      <img src="logo.png" width={210} className="img-fluid mx-auto d-block"/>
+      <img src="logo.png" width={210} className="img-fluid mx-auto d-block" alt="Main-Logo" />
     </div>
   )
 }
